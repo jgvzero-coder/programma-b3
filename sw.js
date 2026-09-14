29f1eff1bc47af9150942b364db39dfb89a17ed1
jgvzero-coder <jgvzero@gmail.com>
2026-09-14 22:24:40 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='programma-b3-v2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
